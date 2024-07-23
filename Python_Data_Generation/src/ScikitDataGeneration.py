58437339c0696c6679d9c71ca6170e01a264e58a
Daniel Wegrzynowski <danielw121099@gmail.com>
2024-07-23 18:52:39 +0200

    Tests: Test calculating estimates with different test data

--- Python_Data_Generation/src/ScikitDataGeneration.py
+++ Python_Data_Generation/src/ScikitDataGeneration.py
@@ -2,3 +2,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
+
+from typing import List, Tuple, Union
+
+
+class RegressionDataGenerator:
+
+    @staticmethod
+    def generate_data(number: int, number_outliers: int) -> Tuple[np.array]:
+        x, y = datasets.make_regression(n_samples=number, n_features=1, n_targets=1, tail_strength=0.3, effective_rank=1, n_informative=1, noise=0.95, bias=100, random_state=5)
+        
+        for i in range(number_outliers):
+            x[-i] = np.random.randint(2, 8) * x[-i]
+
+        x = x * 20000
+
+        y_min, y_max = np.min(y), np.max(y)
+        y_normalized = (y - y_min) / (y_max - y_min)
+
+        y = y_normalized * (2000 + 500) - 500
+
+        outliers = np.random.choice(number, number_outliers, replace=False)
+
+        return np.array(x), np.array(y)
+    
+
+    @staticmethod
+    def save_data(x: np.array, y: np.array, filename: str):
+        df = pd.DataFrame({"X": x.flatten(), "Y": y})
+        df.to_csv(f"Python_Data_Generation/data/{filename}.csv", index=False)
+            
+
+if __name__ == "__main__":
+    x, y = RegressionDataGenerator.generate_data(1000, 20)
+    RegressionDataGenerator.save_data(x, y, "DataLength1000")
--- STM32_Source_Code/src/main.cpp
+++ STM32_Source_Code/src/main.cpp
@@ -11,6 +11,7 @@
 #include "algorithms/HuberEstimator.hpp"
 #include "algorithms/OLSEstimator.hpp"
 
+#include "data/DataLength1000.hpp"
 
 int main(void)
 {
@@ -28,35 +29,27 @@ int main(void)
   static Timer100us timer = Timer100us();
   timer.Init();
 
-  std::array<Point<int>, 5> points_int = {{
-        {1, 2},
-        {2, 3},
-        {3, 4},
-        {4, 5},
-        {5, 6},
-    }};
-  
   while (1)
   {
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 
-    timer.StartMeasurement();
-    auto slope_intercept_TheilSen = CalculateTheilSenEstimator(points_int);
-    auto time_TheilSen = timer.StopMeasurement();
-    logger.LogResults("Theil-Sen", std::to_string(slope_intercept_TheilSen.first), std::to_string(slope_intercept_TheilSen.second), time_TheilSen);
+    // timer.StartMeasurement();
+    // auto slope_intercept_TheilSen = CalculateTheilSenEstimator(points_cut);
+    // auto time_TheilSen = timer.StopMeasurement();
+    // logger.LogResults("Theil-Sen", std::to_string(slope_intercept_TheilSen.first), std::to_string(slope_intercept_TheilSen.second), time_TheilSen);
 
     timer.StartMeasurement();
-    auto slope_intercept_RANSAC = CalculateRansacEstimator(points_int, 10, 1);
+    auto slope_intercept_RANSAC = CalculateRansacEstimator(points, 100, 0.1);
     auto time_RANSAC = timer.StopMeasurement();
     logger.LogResults("RANSAC", std::to_string(slope_intercept_RANSAC.first), std::to_string(slope_intercept_RANSAC.second), time_RANSAC);
 
     timer.StartMeasurement();
-    auto slope_intercept_Huber = CalculateHuberEstimator(points_int, 1);
+    auto slope_intercept_Huber = CalculateHuberEstimator(points, 1.35);
     auto time_Huber = timer.StopMeasurement();
     logger.LogResults("Huber", std::to_string(slope_intercept_Huber.first), std::to_string(slope_intercept_Huber.second), time_Huber);
 
     timer.StartMeasurement();
-    auto slope_intercept_OLS = CalculateOLSEstimator(points_int);
+    auto slope_intercept_OLS = CalculateOLSEstimator(points);
     auto time_OLS = timer.StopMeasurement();
     logger.LogResults("OLS", std::to_string(slope_intercept_OLS.first), std::to_string(slope_intercept_OLS.second), 1);
 
